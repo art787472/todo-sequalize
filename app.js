1681dc8c66ddcff38acd780a522b81bd0b09d081
Chen Yi-Cheng <art787472@gmail.com>
2022-09-11 19:07:25 +0800

    feat: add new and edit page

--- app.js
+++ app.js
@@ -4,13 +4,40 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+const session = require('express-session')
+const usePasssport = require('./config/passport')
+const passport = require('passport')
+
+const routes = require('./routes')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+usePassport(app)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('hello world')
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
+
+app.use('routes')
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
